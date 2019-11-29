825ba408a05352ea2932c3f99e06dc22d17b7346
Amanda Aguilar-Mille <amanda.aguilar-mille@i31470-dnr.fastel.com>
2019-11-29 12:14:49 -0700

    added light and dark theme

--- app/index.js
+++ app/index.js
@@ -1,29 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css'
+import "./index.css";
 
-import Battle from "./components/battle"
-import Popular from "./components/Popular"
-
-
+import Battle from "./components/battle";
+import Popular from "./components/Popular";
+import { ThemeProvider } from "./components/context/Theme";
+import Nav from "./components/Nav";
 //Component
 //State
 //Lifecycle
 //UI
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === "light" ? "dark" : "light"
+        }));
+      }
+    };
+  }
   render() {
     return (
-    <div className="container">
-        <Battle />
-    </div>
-    )
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+
+            <Battle />
+          </div>
+        </div>
+      </ThemeProvider>
+    );
   }
 }
 
-
-ReactDOM.render(
-    <App />, 
-    document.getElementById('app')
-)
-
+ReactDOM.render(<App />, document.getElementById("app"));
